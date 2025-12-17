f55e362db903b2b6883ee5a96a1e647d89138a77
KawyaT <tsamarathunga477@gmail.com>
2025-12-17 23:08:42 +0530

    frontend is done

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import home from './home/home';
+import { Routes, Route } from "react-router-dom"
+
+
+function App() {
+
+  return (
+      <div className='w-full'>
+        <home/>
+        
+        
+      </div>
+  );
+}
+
+export default App;
